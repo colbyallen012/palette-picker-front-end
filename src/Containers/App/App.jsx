c3c58201f93b85be0f0ffeeea9722db6e224d9b6
Colby Allen <colbyallen012@gmail.com>
2019-08-21 15:11:30 -0600

    setup base components
    Co-authored-by: Nathan Frohelich <nathan.froeh@gmail.com>

---
+++ src/Containers/App/App.jsx
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import './App.scss'
+
+class App extends Component {
+  constructor () {
+    super ()
+    this.state = {}
+
+  }
+  render () {
+    return (
+      <div className='App'>
+        <h1>The Palette Picker</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
