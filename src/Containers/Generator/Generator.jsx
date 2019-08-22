69bada37277e97b68b0f51be6fb75d8969760787
Nathan <nathan.froeh@gmail.com>
2019-08-21 18:49:02 -0600

    fix random color render

--- src/Containers/Generator/Generator.jsx
+++ src/Containers/Generator/Generator.jsx
@@ -7,33 +7,38 @@ class Generator extends Component {
   constructor () {
     super ();
     this.state = {
-      randomColors: ['#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF', '#FFFFFF']
+      randomColors: []
     }
   }
 
-  getRandomColor = () => {
-    for(var i = 0; i < 5; i++) {
-      this.setState({randomColors: [...this.state.randomColors, RandomColor()]})
-    }
+  
 
-    console.log(this.state.randomColors);
+  getRandomColor = () => {
+    let newRandomColors = []
+    for(var i = 0; i < 5; i++) {
+      newRandomColors.push(RandomColor())
+    }
+    this.setState({randomColors: newRandomColors})
+    return this.state.randomColors;
   }
 
   clearRandomColor = () => {
-    this.setState.randomColors = []
+    this.setState({randomColors: []})
+    this.getRandomColor()
   }
 
 
   render () {
-   const displayRandomColors = this.state.randomColors.map(color => {
-      return <Color hexCode={color}/>
+   const displayRandomColors = this.state.randomColors.map((color, index) => {
+      return <Color hexCode={color} key={index}/>
     })
   
     return (
       <div className='generator'>
         <h2>Palette Generator Goes here</h2>
-        <button onClick={() => {this.clearRandomColor() 
-          this.getRandomColor()}}>Generate New Palette</button>
+        <button onClick={this.clearRandomColor}>
+          Generate New Palette
+        </button>
         {displayRandomColors}
         {/* {this.state.randomColors.length > 0 && <Color hexCode={this.state.randomColor[0]}/>} */}
       </div>
