f50fad607c828dcbdf15b12b8dcfa3feffb0b800
Colby Allen <colbyallen012@gmail.com>
2019-08-22 15:44:49 -0600

    toggle lock and unlocked words on screen
    Co-authored-by: Nathan Frohelich <nathan.froeh@gmail.com>

--- src/Containers/Generator/Generator.jsx
+++ src/Containers/Generator/Generator.jsx
@@ -41,7 +41,7 @@ export class Generator extends Component {
   toggleLock = (id) => {
     const newState = this.state.randomColors.map((card, index) => {
       if(index+1 === id) {
-        return {color: 'hello', isLocked: !card.isLocked}
+        return {color: card.color, isLocked: !card.isLocked}
       }
       return card
     })
@@ -57,11 +57,11 @@ export class Generator extends Component {
           Generate New Palette
         </button>
         <section className='color--container'>
-          <Color hexCode={this.state.randomColors[0].color} key={1} id={1} toggleLock={this.toggleLock}/>
-          <Color hexCode={this.state.randomColors[1].color} key={2} id={2} toggleLock={this.toggleLock}/>
-          <Color hexCode={this.state.randomColors[2].color} key={3} id={3} toggleLock={this.toggleLock}/>
-          <Color hexCode={this.state.randomColors[3].color} key={4} id={4} toggleLock={this.toggleLock}/>
-          <Color hexCode={this.state.randomColors[4].color} key={5} id={5} toggleLock={this.toggleLock}/>
+          <Color hexCode={this.state.randomColors[0].color} key={1} id={1} toggleLock={this.toggleLock} isLocked={this.state.randomColors[0].isLocked}/>
+          <Color hexCode={this.state.randomColors[1].color} key={2} id={2} toggleLock={this.toggleLock} isLocked={this.state.randomColors[1].isLocked}/>
+          <Color hexCode={this.state.randomColors[2].color} key={3} id={3} toggleLock={this.toggleLock} isLocked={this.state.randomColors[2].isLocked}/>
+          <Color hexCode={this.state.randomColors[3].color} key={4} id={4} toggleLock={this.toggleLock} isLocked={this.state.randomColors[3].isLocked}/>
+          <Color hexCode={this.state.randomColors[4].color} key={5} id={5} toggleLock={this.toggleLock} isLocked={this.state.randomColors[4].isLocked}/>
         </section>
         <form action="">
           <select name="Select project">
