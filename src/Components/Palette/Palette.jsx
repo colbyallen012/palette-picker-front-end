7a66d12e637df833fd023ab14bf4bc59efbea60f
Nathan <nathan.froeh@gmail.com>
2019-08-24 14:18:01 -0600

    Add delete palette functionality

--- src/Components/Palette/Palette.jsx
+++ src/Components/Palette/Palette.jsx
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
-import './Palette.scss'
+import './Palette.scss';
+import { deletePalette } from '../../ApiCalls';
+
 
 export class Palette extends Component {
   
+  handleDelete = (e) => {
+    e.preventDefault()
+    deletePalette(this.props.palette_id)
+  }
+
   render() {
     return (
       <div className='project-palette'>
         <h4 className='tileTitle'>{this.props.name}</h4>
+        <button onClick={this.handleDelete}>Delete Palette</button>
         <p className='tile' style={{backgroundColor: `${this.props.color_1}`}}>{this.props.color_1}</p>
         <p className='tile' style={{backgroundColor: `${this.props.color_2}`}}>{this.props.color_2}</p>        
         <p className='tile' style={{backgroundColor: `${this.props.color_3}`}}>{this.props.color_3}</p>        
