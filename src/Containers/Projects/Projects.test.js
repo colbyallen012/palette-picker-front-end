0f58ac10b8fc701a5c7c8180f734df7f09c0f4d3
Colby Allen <colbyallen012@gmail.com>
2019-08-25 13:47:35 -0600

    update snapshot, add handleProjectName test

---
+++ src/Containers/Projects/Projects.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Projects from './Projects';
+
+describe('Projects', () => {
+  let wrapper, instance;
+  const props = {
+    projects: [],
+    name:'project name',
+    id: 1,
+    palettes: [],
+    deleteProject: jest.fn(),
+    deletePalette: jest.fn()
+  }
+
+  beforeEach(() => {
+    wrapper = shallow(<Projects {...props}/>)
+    instance = wrapper.instance()
+
+  })
+
+  it('should match snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should change state when handleProjectName is invoked', () => {
+    const mockEvent = { target: { newProjectName: 'title', value: 'Project Name'} };
+    const expected = 'Project Name';
+    wrapper.instance().handleProjectName(mockEvent);
+    expect(wrapper.state('newProjectName')).toEqual(expected)
+  })
+
+  it.skip('should run postProject when handleSubmitProject is called', () => {
+    const currentState = {newProjectName:'Project Name'};
+    wrapper.instance().submitIdea({preventDefault: jest.fn()});
+  })
+})
