0acbdcef51d5ff6031f01ff45b4d384603f4f4f8
Colby Allen <colbyallen012@gmail.com>
2019-08-25 19:02:29 -0600

    setup card container test

---
+++ src/Containers/CardContainer/CardContatiner.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import CardContainer from './CardContainer';
+
+describe('Card Container', () => {
+  let wrapper;
+  const mockColors = [{
+    color_1: 'test',
+    color_2: 'test',
+    color_3: 'test',
+    color_4: 'test',
+    color_5: 'test'
+  }]
+
+  beforeEach(() => {
+    wrapper = shallow(<CardContainer hexCode={mockColors}/>)
+  })
+
+  it('should match snapshot', () => {
+    expect(wrapper).toMatchSnapshot()
+  }); 
+})
+
