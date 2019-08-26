f0173239f589ef9276e670c9a48e623013e5a51a
Colby Allen <colbyallen012@gmail.com>
2019-08-26 13:32:37 -0600

    add happy initial test for fetchPalettesById

--- src/ApiCalls.test.js
+++ src/ApiCalls.test.js
@@ -7,7 +7,33 @@ describe('ApiCalls', () => {
   })
   
   describe('Get palettes by id', () => {
+    let mockPalette;
 
+    beforeEach(() => {
+      mockPalette = [{
+          id: 3,
+          project_id: 1,
+          name: "palette 3",
+          color_1: "31393C",
+          color_2: "2176FF",
+          color_3: "33A1FD",
+          color_4: "FDCA40",
+          color_5: "F79824",
+      }]
+
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          json: () => Promise.resolve(mockPalette)
+        })
+      })
+    })
+
+    it('should be called with the correct url', () => {
+      const id = 1;
+      const expected = `http://localhost:3001/api/v1/projects/${id}/palettes`;
+      fetchPalettesById(id);
+      expect(window.fetch).toHaveBeenCalledWith(expected)
+    })
   })
 
   describe('Post palettes', () => {
