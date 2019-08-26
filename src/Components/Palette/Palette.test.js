6c9fcdca0e69c06501635fcbb8d8f03351a9e2d3
Colby Allen <colbyallen012@gmail.com>
2019-08-25 19:21:07 -0600

    add test for handle delete in palette test

--- src/Components/Palette/Palette.test.js
+++ src/Components/Palette/Palette.test.js
@@ -5,9 +5,22 @@ import Palette from './Palette';
 describe('Palette', () => {
   let wrapper, instance;
 
+  const props = {
+    deletePalette: jest.fn(),
+  }
+
   beforeEach(() => {
-    wrapper = shallow (<Palette />)
+    wrapper = shallow (<Palette {...props}/>)
     instance = wrapper.instance()
+  })
+
+  it('should match snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  }); 
+
+  it('should call deletePalette from state when handleDelete is called', () => {
+    wrapper.instance().handleDelete({preventDefault: jest.fn()});
+
   })
 
 })
