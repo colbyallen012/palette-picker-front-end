1fd0f2ddd9d1065e2a8e3a6c2fb3e228ba151da9
Nathan <nathan.froeh@gmail.com>
2019-08-26 17:33:45 -0600

    Add generator snapshot

--- src/Containers/Generator/Generator.test.js
+++ src/Containers/Generator/Generator.test.js
@@ -4,9 +4,26 @@ import Generator from './Generator';
 
 describe('Generator', () => {
   let wrapper, instance;
+  const projects = [{name: 'Bat Cave'}]
+  const palettes = [{
+    name: 'floor',
+    color_1: '#000000',
+    color_2: '#000000',
+    color_3: '#000000',
+    color_4: '#000000',
+    color_5: '#000000'
+  }]
+  const props = {
+    projects: projects,
+    getAllPalettes: jest.fn(() => palettes)
+  }
   beforeEach(() => {
-    wrapper = shallow(<Generator/>)
+    wrapper = shallow(<Generator {...props}/>)
     instance = wrapper.instance()
+  })
+
+  it('Generator should match snapshot', () => {
+    expect(wrapper).toMatchSnapshot()
   })
 
 
