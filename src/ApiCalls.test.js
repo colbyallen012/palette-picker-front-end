fc62bf9f137aa6cd41a6591e68c4848befd222aa
Colby Allen <colbyallen012@gmail.com>
2019-08-26 13:25:20 -0600

    setup apiCalls test

---
+++ src/ApiCalls.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {fetchAllProjects, fetchPalettesById, fetchAllPalettes, postPalette, postProject, patchProject, patchPalette, deleteProject, deletePalette} from './ApiCalls'
+
+describe('ApiCalls', () => {
+  describe('Get projects', () => {
+
+  })
+  
+  describe('Get palettes by id', () => {
+
+  })
+
+  describe('Post palettes', () => {
+    
+  })
+
+  describe('Post projects', () => {
+    
+  })
+
+  describe('Patch projects', () => {
+    
+  })
+
+  describe('Patch palette', () => {
+    
+  })
+
+  describe('Delete project', () => {
+    
+  })
+
+  describe('Delete palette', () => {
+    
+  })
+})
