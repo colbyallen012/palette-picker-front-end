40575df0cb8f1d4efcfbe832abe8edf8e3c01fe4
Nathan <nathan.froeh@gmail.com>
2019-08-26 17:22:07 -0600

    Add Generator test file

---
+++ src/Containers/Generator/Generator.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import Generator from './Generator';
+
+describe('Generator', () => {
+  let wrapper, instance;
+  beforeEach(() => {
+    wrapper = shallow(<Generator/>)
+    instance = wrapper.instance()
+  })
+
+
+})
