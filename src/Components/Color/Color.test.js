6268ab2881ed1cbd12095438d4ead51d5655a1ce
Nathan <nathan.froeh@gmail.com>
2019-08-27 14:52:23 -0600

    Add lock click should call toggleLock from locked test

--- src/Components/Color/Color.test.js
+++ src/Components/Color/Color.test.js
@@ -20,7 +20,20 @@ describe('Color', () => {
     expect(wrapper).toMatchSnapshot();
   })
 
-  it('lock click should call toggleLock', () => {
+  it('lock click should call toggleLock from unlocked', () => {
+    expect(props.toggleLock).toHaveBeenCalledTimes(0)
+    wrapper.find('input').simulate('click')
+    expect(props.toggleLock).toHaveBeenCalledTimes(1)
+  })
+
+  it('lock click should call toggleLock from locked', () => {
+    const props = {
+      hexCode: 'FFFFFF',
+      id: 1,
+      toggleLock: jest.fn(),
+      isLocked: true
+    }
+    wrapper = shallow (<Color {...props}/>)
     expect(props.toggleLock).toHaveBeenCalledTimes(0)
     wrapper.find('input').simulate('click')
     expect(props.toggleLock).toHaveBeenCalledTimes(1)
