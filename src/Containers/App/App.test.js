4347232d3d1b9ec7dac341702e27f5030e0e6bd8
Nathan <nathan.froeh@gmail.com>
2019-08-26 19:21:55 -0600

    Add App test file

--- src/Containers/App/App.test.js
+++ src/Containers/App/App.test.js
@@ -1,1 +1,13 @@
 
+import React from 'react';
+import {shallow} from 'enzyme';
+import App from './App';
+
+describe('App', () => {
+  let wrapper, instance;
+
+  beforeEach(() => {
+    wrapper = shallow(<App/>)
+    instance = wrapper.instance()
+  })
+})
