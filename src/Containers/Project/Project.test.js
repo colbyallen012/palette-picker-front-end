0798d58baeb51033c985bd235f6d0e9eb494f21c
Nathan <nathan.froeh@gmail.com>
2019-08-26 20:10:08 -0600

    Add snapshot test

--- src/Containers/Project/Project.test.js
+++ src/Containers/Project/Project.test.js
@@ -3,5 +3,30 @@ import {shallow} from 'enzyme';
 import {Project} from './Project';
 
 describe('Project', () => {
+  let wrapper
+  const projects = [{name: 'Bat Cave'}]
+  const palettes = [{
+    name: 'floor',
+    color_1: '#000000',
+    color_2: '#000000',
+    color_3: '#000000',
+    color_4: '#000000',
+    color_5: '#000000'
+  }]
+  const props = {
+    projects: projects,
+    palettes: palettes,
+    deleteProject: jest.fn(),
+    deletePalette: jest.fn(),
+    getAllProjects: jest.fn()
+  }
+  beforeEach(() => {
+    wrapper = shallow(<Project{...props}/>)
+  })
+
+  it('Project should match snapshot', () => {
+    expect(wrapper).toMatchSnapshot()
+  })
+
 
 })
