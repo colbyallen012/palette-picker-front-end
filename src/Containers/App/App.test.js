8fec881d6baad726facfe158a4c77361b6d41112
Nathan <nathan.froeh@gmail.com>
2019-08-26 19:58:14 -0600

    Add unfinished getAllProjects test

--- src/Containers/App/App.test.js
+++ src/Containers/App/App.test.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import App from './App';
-
+import {fetchAllProjects} from '../../ApiCalls'
 describe('App', () => {
   let wrapper, instance;
 
@@ -13,5 +13,13 @@ describe('App', () => {
 
   it('should match snapshot', () => {
     expect(wrapper).toMatchSnapshot()
+  })
+
+  xit('getAllProjects should call fetchAllProjects', async () => {
+    const spy = jest.spyOn(fetchAllProjects())
+    instance.fetchAllProjects = jest.fn()
+    expect(spy).toHaveBeenCalledTimes(0)
+    await instance.getAllProjects()
+    expect( spy).toHaveBeenCalledTimes(1)
   })
 })
