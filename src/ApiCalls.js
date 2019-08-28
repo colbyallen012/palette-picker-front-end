b919fb759e011b5e7071e45f56ed148b5a503784
Nathan <nathan.froeh@gmail.com>
2019-08-28 13:21:34 -0600

    Replace fetch url with live api link

--- src/ApiCalls.js
+++ src/ApiCalls.js
@@ -1,19 +1,19 @@
 export const fetchAllProjects = () => {
-  return fetch('http://localhost:3001/api/v1/projects')
+  return fetch('https://the-palette-picker.herokuapp.com/api/v1/projects')
     .then(res => res.json())
     .then(res => res)
     .catch (error => error.message)   
 }
 
 export const fetchPalettesById = (id) => {
-  return fetch(`http://localhost:3001/api/v1/projects/${id}/palettes`)
+  return fetch(`https://the-palette-picker.herokuapp.com/api/v1/projects/${id}/palettes`)
     .then(res => res.json())
     .then(data => data)
     .catch(error => error.message)
 }
 
 export const fetchAllPalettes = () => {
-  return fetch(`http://localhost:3001/api/v1/palettes`)
+  return fetch(`https://the-palette-picker.herokuapp.com/api/v1/palettes`)
     .then(res => res.json())
     .then(data => data)
     .catch(error => error.message)
@@ -27,7 +27,7 @@ export const postPalette = (palette) => {
       "Content-Type": "application/json"
     }
   }
-  return fetch('http://localhost:3001/api/v1/palettes', options)
+  return fetch('https://the-palette-picker.herokuapp.com/api/v1/palettes', options)
     .then(res => res.json())
     .then(data => data)
     .catch(error => error.message)
@@ -41,7 +41,7 @@ export const postProject = (project) => {
       "Content-Type": "application/json"
     }
   }
-  return fetch('http://localhost:3001/api/v1/projects', options)
+  return fetch('https://the-palette-picker.herokuapp.com/api/v1/projects', options)
     .then(res => res.json())
     .then(data => data)
     .catch(error => error.message)
@@ -55,7 +55,7 @@ export const patchProject = (project) => {
       "Content-Type": "application/json"
     }
   }
-  return fetch(`http://localhost:3001/api/v1/projects/${project}`, options)
+  return fetch(`https://the-palette-picker.herokuapp.com/api/v1/projects/${project}`, options)
   .then(res => res.json())
   .then(data => data)
   .catch(error => error.message)
@@ -69,7 +69,7 @@ export const patchPalette = (palette) => {
       "Content-Type": "application/json"
     }
   }
-  return fetch(`http://localhost:3001/api/v1/palettes/${palette}`, options)
+  return fetch(`https://the-palette-picker.herokuapp.com/api/v1/palettes/${palette}`, options)
   .then(res => res.json())
   .then(data => data)
   .catch(error => error.message)
@@ -82,7 +82,7 @@ export const deleteProject = (project) => {
       "Content-Type": "application/json"
     }
   }
-  return fetch(`http://localhost:3001/api/v1/projects/${project}`, options)
+  return fetch(`https://the-palette-picker.herokuapp.com/api/v1/projects/${project}`, options)
   .then(res => res.json())
   .then(data => console.log(data))
   .catch(error => error.message)
@@ -95,7 +95,7 @@ export const deletePalette = (palette) => {
       "Content-Type": "application/json"
     }
   }
-  return fetch(`http://localhost:3001/api/v1/palettes/${palette}`, options)
+  return fetch(`https://the-palette-picker.herokuapp.com/api/v1/palettes/${palette}`, options)
   .then(res => res.json())
   .then(data => data)
   .catch(error => error.message)
